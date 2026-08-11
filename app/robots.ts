a06211412a4bdf7102d85851732fe2301020dd31
Kissprisonblock2018 <Join@4goodvibes.shop>
2026-08-11 02:14:26 -0400

    Revert "feat(blog): SEO blog with automated authority publishing pipeline"
    This reverts commit 65b8fd3e9107f786c9f9b321c6f90e03f39f4c7c.

--- app/robots.ts
+++
@@ -1,9 +0,0 @@
-import type { MetadataRoute } from "next";
-import { siteOrigin } from "@/lib/util";
-
-export default function robots(): MetadataRoute.Robots {
-  return {
-    rules: [{ userAgent: "*", allow: "/", disallow: ["/admin", "/api"] }],
-    sitemap: `${siteOrigin()}/sitemap.xml`,
-  };
-}
